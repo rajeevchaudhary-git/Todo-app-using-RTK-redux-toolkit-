7e24cf780f7f01b9f623801516633c29c77fd8a0
rajeevchoudhary067@gmail.com <91446278+rajeevchoudhary035@users.noreply.github.com>
2024-05-30 22:39:19 +0530

    ToDo App by redux

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todo"
+
+
+
+function App() {
+
+  return (
+   <>
+   <h2 className="text-center text-2xl text-green-800 mt-1">Learn with redux toolkit </h2>
+   <AddTodo/>
+   <Todo/>
+   </>
+  )
+}
+
+export default App
